12be2c67b2916867b02961efd98176eca60ff590
JenniferMrm <jennifer-miramon_student2021@wilder.school>
2021-04-13 11:03:31 +0200

    correction du layout, ajout des chemins d'import de base

--- wildbook/src/App.js
+++ wildbook/src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import './App.css';
 import Main from './layouts/Main/Main';
-import Home from './view/Home/Home';
+import Profil from './view/Profil/Profil';
 
 
 function App() {
   return (
     <Main>
       {/* <Home /> */}
-      {/* <Login /> */}
+      {/* <Login />*/}
+      {/* <Profil /> */}
     </Main>
   );
 }
