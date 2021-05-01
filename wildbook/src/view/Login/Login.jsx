3de917c45789d528ae5328a25840325a53098eb5
yoann189.labbe@laposte.net <y.labbe.contact@gmail.comgit log -pwild-gitls$ lspwd~cdgit config --global user.email y.labbe.contact@gmail.com>
2021-05-01 16:57:46 +0200

    MAJ

--- wildbook/src/view/Login/Login.jsx
+++ wildbook/src/view/Login/Login.jsx
@@ -1,11 +1,6 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
-import CreateAccount from "./CreateAccount";
-import { Link } from "react-router-dom";
-import Load from "./Load";
-import DialogNotif from "../Notif/DialogNotif";
-import { Dialog } from "@material-ui/core";
 import Popupload from "./Popupload";
 import Popupaccount from "./Popupaccount";
 
